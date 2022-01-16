fe71e287f0c8e1123ba9bb2aafa3f2e3cd025ba6
vasindran <61245148+vasindran@users.noreply.github.com>
2022-01-16 21:54:37 +0000

    adding terraform files

---
+++ Terraform/variable.tf
@@ -0,0 +1,9 @@
+variable "sites_BradyWather_name" {
+  "value"       = null
+  description   = "Prefix of the resource group name that's combined with a random ID so name is unique in your Azure subscription."
+}
+
+variable "serverfarms_ASP_devopsinterview_92c5_externalid" {
+  "value" = null
+  description   = "Location of the resource group."
+}
